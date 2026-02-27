53abd85ce2a9752c0489d525f8680176875a3e00
pierrefrangoat <lou2deplus@gmail.com>
2026-02-27 21:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+<script>
 
+    function showChapter(id) {
+    document.querySelectorAll('.chapter')
+        .forEach(c => c.classList.remove('active'));
+    const target = document.getElementById(id);
+    if (target) target.classList.add('active');
+        }
+    
+</script>
+    
